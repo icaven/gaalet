6d24ae46ac0c47d6c6ed57e57926e80ddb92ff1b
groebiator <groebiator>
2010-08-16 15:40:13 +0000

    only inverse of versor implemented
    git-svn-id: https://svn.code.sf.net/p/gaalet/code/trunk@3 d266722a-31f7-4b50-8c79-7894b272e5be

--- include/cpp0x/inverse.h
+++ include/cpp0x/inverse.h
@@ -6,19 +6,79 @@
 
 namespace gaalet {
 
+//check for versor
+template<typename CL, bool parent_even_grade = (BitCount<CL::head>::value%2)>
+struct check_versor
+{
+   static const bool even_grade = (BitCount<CL::head>::value%2);
+   static const bool value = (parent_even_grade==even_grade) ? check_versor<typename CL::tail, even_grade>::value : false;
+};
+template<bool parent_even_grade>
+struct check_versor<cl_null, parent_even_grade>
+{
+   static const bool even_grade = parent_even_grade;
+   static const bool value = true;
+};
+
+//go through inversion evaluation type checks
+//value=1 - versor inversion
+template<class A>
+struct inverse_evaluation_type
+{
+   static const int value = (check_versor<typename A::clist>::value) ? 1 :
+                            -1;
+};
+
+template<class A, int ET = inverse_evaluation_type<A>::value>
+struct inverse : public expression<inverse<A>>
+{
+   static_assert(ET!=-1, "no method for evaluating this type of multivector implemented");
+};
+
+//versor inversion
+template<class A>
+struct inverse<A, 1> : public expression<inverse<A>>
+{
+   typedef typename A::clist clist;
+
+   typedef typename A::metric metric;
+
+   inverse(const A& a_)
+      :  a(a_),
+         div(1.0/((~a)*a).template element<0x00>())
+   { }
+
+   template<conf_t conf>
+   element_t element() const {
+      return a.element<conf>() * div;
+   }
+
+protected:
+   const A& a;
+   element_t div;
+};
+
+
 }  //end namespace gaalet
+
+template <class A> inline
+gaalet::inverse<A>
+operator!(const gaalet::expression<A>& a) {
+   return gaalet::inverse<A>(a);
+}
+
 
 /*template <class A> inline
 auto operator!(const gaalet::expression<A>& a) -> decltype((~a)*(1.0/(a*(~a)).template element<0x00>()))
 {
    return (~a)*(1.0/(a*(~a)).template element<0x00>());
-}*/
+}
 
 template <class A> inline
 auto operator!(const gaalet::expression<A>& a) -> decltype(eval(a*gaalet::element_t()))
 {
    gaalet::element_t div = 1.0/((~a)*a).template element<0x00>();
    return eval(a*div);
-}
+}*/
 
 #endif
