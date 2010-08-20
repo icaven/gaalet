efa6604f5c27c4f14028b64d7d657fe426e1f66e
groebiator <groebiator>
2010-08-20 15:38:51 +0000

    still won't work
    git-svn-id: https://svn.code.sf.net/p/gaalet/code/trunk@11 d266722a-31f7-4b50-8c79-7894b272e5be

--- cpp/tests/testMonteCarloPiCuda.cu
+++ cpp/tests/testMonteCarloPiCuda.cu
@@ -4,40 +4,45 @@
 
 typedef gaalet::algebra<gaalet::signature<4,1> > cm;
 
-__device__ float d_pi = 0.0;
+__device__ __shared__ unsigned int d_n_s;
 
 __global__ void test()
 {
-   double r = (double)blockDim.x;
+   __shared__ double r;
    
-   unsigned int n_q = blockDim.x*blockDim.y*blockDim.z;
-   __shared__ unsigned int n_s;
-   if(threadIdx.x == 0 && threadIdx.y==0 && threadIdx.z==0) n_s = 0;
+   __shared__ cm::mv<0x01>::type e1;
+   __shared__ cm::mv<0x02>::type e2;
+   __shared__ cm::mv<0x04>::type e3;
+   __shared__ cm::mv<0x08>::type ep;
+   __shared__ cm::mv<0x10>::type em;
+   __shared__ cm::mv<0x00>::type one;
 
-   cm::mv<0x01>::type e1(1.0);
-   cm::mv<0x02>::type e2(1.0);
-   cm::mv<0x04>::type e3(1.0);
-   cm::mv<0x08>::type ep(1.0);
-   cm::mv<0x10>::type em(1.0);
+   __shared__ cm::mv<0x08, 0x10>::type e0;
+   __shared__ cm::mv<0x08, 0x10>::type einf;
 
-   cm::mv<0x00>::type one(1.0);
+   __shared__ cm::mv<0x08, 0x10>::type S;
 
-   cm::mv<0x08, 0x10>::type e0 = 0.5*(em-ep);
-   cm::mv<0x08, 0x10>::type einf = em+ep;
-
-   cm::mv<0x08, 0x10>::type S = e0 - 0.5*r*r*einf;
-
+   if(threadIdx.x == 0 && threadIdx.y==0 && threadIdx.z==0) {
+      d_n_s = 0;
+      r = (double)blockDim.x;
+      e1[0] = 1.0;
+      e2[0] = 1.0;
+      e3[0] = 1.0;
+      em[0] = 1.0;
+      ep[0] = 1.0;
+      one[0] = 1.0;
+      e0 = 0.5*(em-ep);
+      einf = em+ep;
+      S = e0 - 0.5*r*r*einf;
+   }
 
    cm::mv<0x01, 0x02, 0x04>::type x = ((double)threadIdx.x*e1 + (double)threadIdx.y*e2 + (double)threadIdx.z*e3)*r;
    cm::mv<0x01, 0x02, 0x04, 0x08, 0x10>::type P = x + 0.5*(x&x)*einf + e0;
    double d = eval(S&P);
    if(d>=0.0) {
       //atomicAdd(&n_s, 1);
-      ++n_s;
+      ++d_n_s;
    }
-   __syncthreads();
-   
-   if(threadIdx.x == 0 && threadIdx.y==0 && threadIdx.z==0) d_pi = 6.0*(float)n_s/(float)n_q;
 }
 
 
@@ -45,16 +50,32 @@ int main()
 {
    std::cout << "Hello Gaalet Monte Carlo on Cuda!" << std::endl;
 
+   cudaSetDevice(0);
+   cudaError_t error;
 
-   dim3 threads( 1, 1, 1 );
+   unsigned int r = 5;
+   dim3 threads( r, r, r );
 
    test <<< 1, threads >>>();
-
-   float pi;
-   if(cudaMemcpyFromSymbol(&pi, "d_pi", sizeof(pi), 0, cudaMemcpyDeviceToHost) != cudaSuccess) {
-      std::cout << "cudaMemcpyFromSymbol() not succeded!" << std::endl;
+   cudaThreadSynchronize();
+   error = cudaGetLastError();
+   if(error != cudaSuccess) 
+   {
+      std::cerr << "Cuda error: " << cudaGetErrorString(error) << std::endl;
    }
+
+   unsigned int n_s;
+   cudaMemcpyFromSymbol(&n_s, "d_n_s", sizeof(n_s), 0, cudaMemcpyDeviceToHost);
+   error = cudaGetLastError();
+   if(error != cudaSuccess) 
+   {
+      std::cerr << "Cuda error: " << cudaGetErrorString(error) << std::endl;
+   }
+
+   unsigned int n_q = r*r*r;
+   double pi = 6.0*(double)n_s/(double)n_q;
+
    std::cout << "Pi: " << pi << std::endl;
-   
+
    cudaThreadExit();
 }
