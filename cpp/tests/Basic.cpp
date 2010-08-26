3b41962d47d66d2d9c0a99de41fda47c91eeae39
groebiator <groebiator>
2010-08-26 08:15:17 +0000

    added CMakeLists.txt files to parent directories
    git-svn-id: https://svn.code.sf.net/p/gaalet/code/trunk@34 d266722a-31f7-4b50-8c79-7894b272e5be

--- cpp/tests/Basic.cpp
+++ cpp/tests/Basic.cpp
@@ -1,16 +1,17 @@
 #include "gaalet.h"
 
+typedef gaalet::algebra<gaalet::signature<0,0> > em;
 
 int main()
 {
-   gaalet::mv<3, 5>::type a = {1.0, 2.0};
-   gaalet::mv<3, 4>::type b = {4.0, 7.0};
+   em::mv<3, 5>::type a(1.0, 2.0);
+   em::mv<3, 4>::type b(4.0, 7.0);
 
-   auto c = a - b;
+   typeof(a-b) c = a - b;
 
    std::cout << "a: " << a << std::endl;
    std::cout << "b: " << b << std::endl;
    std::cout << "c: " << c << std::endl;
 
-   gaalet::mv<0>::type d;
+   em::mv<0>::type d;
 }
