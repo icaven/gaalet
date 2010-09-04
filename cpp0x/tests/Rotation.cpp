e34549275b444092b2e97b6198a6fb4ca3c3f6ed
groebiator <groebiator>
2010-09-04 10:56:23 +0000

    changed Rotation test
    git-svn-id: https://svn.code.sf.net/p/gaalet/code/trunk@45 d266722a-31f7-4b50-8c79-7894b272e5be

--- cpp0x/tests/Rotation.cpp
+++ cpp0x/tests/Rotation.cpp
@@ -7,10 +7,10 @@ static em::mv<0x04>::type e3 = {1.0};
 
 int main()
 {
-   auto S = (e1^e2)*(0.5*M_PI);
-   auto R = exp(-0.5*S);
-   
-   auto a = 1*e1 + 2*e2 + 3*e3;
+   auto S = -0.5*M_PI*(e1^e2);
+   auto R = exp(0.5*S);
+
+   em::mv<1,2,4>::type a = {1.6, 3.2, 5.5}; 
    auto b = eval(grade<1>(R*a*(~R)));
 
    std::cout << "S: " << S << ", R: " << R << std::endl;
