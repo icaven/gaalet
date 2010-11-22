d92a2b04fed17937dea4fa8acdfee4d72d80b640
groebiator <groebiator>
2010-11-22 23:57:13 +0000

    Ölkj
    git-svn-id: https://svn.code.sf.net/p/gaalet/code/trunk@56 d266722a-31f7-4b50-8c79-7894b272e5be

--- cpp0x/examples/CarDynamics/RungeKuttaClassic.h
+++ cpp0x/examples/CarDynamics/RungeKuttaClassic.h
@@ -3,24 +3,99 @@
 
 #include <tuple>
 
-template<typename... A>
-struct tuple_addition
-{
-   typedef std::tuple<A...> tuple_t;
+//Wrapper class for CRTP
+template <class E>
+struct tuple_expression {
+   operator const E& () const {
+      return *static_cast<const E*>(this);
+   }
+};
 
-   tuple_addition(const tuple_t& l_, const tuple_t& r_)
+template<typename L, typename R>
+struct tuple_addition : public tuple_expression<tuple_addition<L,R>>
+{
+   tuple_addition(const L& l_, const R& r_)
       :  l(l_),
          r(r_)
    { }
 
    template<int I>
-   //auto element() -> decltype(std::get<I>(l) + std::get<I>(r)) {
-   typename std::tuple_element<I, tuple_t>::type element() {
+   auto element() -> decltype(std::get<I>(L()) + std::get<I>(R())) {
+   //typename std::tuple_element<I, tuple_t>::type element() {
       return std::get<I>(l) + std::get<I>(r);
    }
 
-   const tuple_t& l;
-   const tuple_t& r;
+   const L& l;
+   const R& r;
+};
+
+template<typename L>
+struct tuple_scalar_product : public tuple_expression<tuple_scalar_product<L>>
+{
+   tuple_scalar_product(const L& l_, const double& r_)
+      :  l(l_),
+         r(r_)
+   { }
+
+   template<int I>
+   auto element() -> decltype(std::get<I>(L())*double()) {
+   //typename std::tuple_element<I, L>::type element() {
+      return std::get<I>(l)*r;
+   }
+
+   const L& l;
+   double r;
+};
+
+template<class L, class R> inline
+tuple_addition<L, R>
+operator+(const tuple_expression<L>& l, const tuple_expression<R>& r) {
+   return tuple_addition<L, R>(l, r);
+}
+
+template<class L, class... A> inline
+tuple_addition<L, std::tuple<A...>>
+operator+(const tuple_expression<L>& l, const std::tuple<A...>& r) {
+   return tuple_addition<L, std::tuple<A...>>(l, r);
+}
+
+template<class... A, class R> inline
+tuple_addition<std::tuple<A...>, R>
+operator+(const std::tuple<A...>& l, const tuple_expression<R>& r) {
+   return tuple_addition<std::tuple<A...>, R>(l, r);
+}
+
+template<class... L, class... R> inline
+tuple_addition<std::tuple<L...>, std::tuple<R...>>
+operator+(const std::tuple<L...>& l, const std::tuple<R...>& r) {
+   return tuple_addition<std::tuple<L...>, std::tuple<R...>>(l, r);
+}
+
+template<class T> inline
+tuple_scalar_product<T>
+operator*(const tuple_expression<T>& l, const double& r) {
+   return tuple_scalar_product<T>(l, r);
+}
+
+template<class... A> inline
+tuple_scalar_product<std::tuple<A...>>
+operator*(const std::tuple<A...>& l, const double& r) {
+   return tuple_scalar_product<std::tuple<A...>>(l, r);
+}
+
+template<class... A> inline
+tuple_scalar_product<std::tuple<A...>>
+operator*(const double& l, const std::tuple<A...>& r) {
+   return tuple_scalar_product<std::tuple<A...>>(r, l);
+}
+
+template<typename E, typename R, int I=std::length<R>::value>
+struct tuple_expression_evaluation
+{
+   static void operator()(const E& e, R& r)
+   {
+      std::get<I>(r) = e.element<I>();
+   }
 };
 
 #endif
