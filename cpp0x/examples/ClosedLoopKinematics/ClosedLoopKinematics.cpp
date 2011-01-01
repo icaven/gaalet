72c6c2a76d073ea26b108602ce839c97e72d4454
groebiator <groebiator>
2011-01-01 17:48:26 +0000

    transparent spheres
    git-svn-id: https://svn.code.sf.net/p/gaalet/code/trunk@77 d266722a-31f7-4b50-8c79-7894b272e5be

--- cpp0x/examples/ClosedLoopKinematics/ClosedLoopKinematics.cpp
+++ cpp0x/examples/ClosedLoopKinematics/ClosedLoopKinematics.cpp
@@ -10,23 +10,84 @@
 #include <osgGA/TrackballManipulator>
 #include <osg/PositionAttitudeTransform>
 #include <osg/ShapeDrawable>
+#include <osg/Depth>
+#include <osg/Material>
 
 using namespace cga;
 
 int main()
 {
    ::osg::Group* sceneRoot = new ::osg::Group;
+   
+   ::osg::Group* transparentSpheres = new ::osg::Group;
+   sceneRoot->addChild(transparentSpheres);
+   ::osg::StateSet* tsStateSet = transparentSpheres->getOrCreateStateSet();
+   // Enable blending, select transparent bin.
+   tsStateSet->setMode( GL_BLEND, ::osg::StateAttribute::ON );
+   tsStateSet->setRenderingHint( ::osg::StateSet::TRANSPARENT_BIN );
 
-   auto x1 = 1.0*e1;
-   double r = 0.5;
-   cga::osg::Sphere* s1_p = new cga::osg::Sphere(eval(x1 + 0.5*(eval(x1&x1)-r*r)*einf + e0));
-   cga::osg::Sphere& s1 = *s1_p;
+   // Enable depth test so that an opaque polygon will occlude a transparent one behind it.
+   tsStateSet->setMode( GL_DEPTH_TEST, ::osg::StateAttribute::ON );
+ 
+   // Conversely, disable writing to depth buffer so that
+   // a transparent polygon will allow polygons behind it to shine thru.
+   // OSG renders transparent polygons after opaque ones.
+   ::osg::Depth* depth = new ::osg::Depth;
+   depth->setWriteMask( false );
+   tsStateSet->setAttributeAndModes( depth, ::osg::StateAttribute::ON );
 
-   ::osg::ShapeDrawable* s1Drawable = new ::osg::ShapeDrawable(s1_p);
-   ::osg::Geode* s1Geode = new ::osg::Geode();
-   s1Geode->addDrawable(s1Drawable);
+   // Disable conflicting modes.
+   //tsStateSet->setMode( GL_LIGHTING, ::osg::StateAttribute::OFF );
+  
+   ::osg::Material* tsMat = (::osg::Material*)tsStateSet->getAttribute(::osg::StateAttribute::MATERIAL);
+   if(!tsMat) {
+      double opacity = 0.2;
+      tsMat = new ::osg::Material;
+      tsMat->setAlpha(::osg::Material::FRONT_AND_BACK, opacity);
+      tsStateSet->setAttributeAndModes(tsMat,::osg::StateAttribute::ON);
+   }
 
-   sceneRoot->addChild(s1Geode);
+
+   cga::osg::Sphere* s_wbf_p = new cga::osg::Sphere();
+   cga::osg::Sphere& s_wbf = *s_wbf_p;
+   ::osg::ShapeDrawable* s_wbf_Drawable = new ::osg::ShapeDrawable(s_wbf_p);
+   s_wbf_Drawable->setDataVariance(::osg::Object::DYNAMIC);
+   ::osg::Geode* s_wbf_Geode = new ::osg::Geode();
+   s_wbf_Geode->addDrawable(s_wbf_Drawable);
+   transparentSpheres->addChild(s_wbf_Geode);
+
+   cga::osg::Sphere* s_wbr_p = new cga::osg::Sphere();
+   cga::osg::Sphere& s_wbr = *s_wbr_p;
+   ::osg::ShapeDrawable* s_wbr_Drawable = new ::osg::ShapeDrawable(s_wbr_p);
+   s_wbr_Drawable->setDataVariance(::osg::Object::DYNAMIC);
+   ::osg::Geode* s_wbr_Geode = new ::osg::Geode();
+   s_wbr_Geode->addDrawable(s_wbr_Drawable);
+   transparentSpheres->addChild(s_wbr_Geode);
+
+   cga::osg::Sphere* s_mps_p = new cga::osg::Sphere();
+   cga::osg::Sphere& s_mps = *s_mps_p;
+   ::osg::ShapeDrawable* s_mps_Drawable = new ::osg::ShapeDrawable(s_mps_p);
+   s_mps_Drawable->setDataVariance(::osg::Object::DYNAMIC);
+   ::osg::Geode* s_mps_Geode = new ::osg::Geode();
+   s_mps_Geode->addDrawable(s_mps_Drawable);
+   transparentSpheres->addChild(s_mps_Geode);
+
+   cga::osg::Point* p_wc_p = new cga::osg::Point();
+   cga::osg::Point& p_wc = *p_wc_p;
+   ::osg::ShapeDrawable* p_wc_Drawable = new ::osg::ShapeDrawable(p_wc_p);
+   p_wc_Drawable->setDataVariance(::osg::Object::DYNAMIC);
+   ::osg::Geode* p_wc_Geode = new ::osg::Geode();
+   p_wc_Geode->addDrawable(p_wc_Drawable);
+   sceneRoot->addChild(p_wc_Geode);
+
+
+   cga::osg::Point* p_sa_p = new cga::osg::Point();
+   cga::osg::Point& p_sa = *p_sa_p;
+   ::osg::ShapeDrawable* p_sa_Drawable = new ::osg::ShapeDrawable(p_sa_p);
+   p_sa_Drawable->setDataVariance(::osg::Object::DYNAMIC);
+   ::osg::Geode* p_sa_Geode = new ::osg::Geode();
+   p_sa_Geode->addDrawable(p_sa_Drawable);
+   sceneRoot->addChild(p_sa_Geode);
 
 
    osgViewer::Viewer viewer;
@@ -42,6 +103,8 @@ int main()
    {
       viewer.realize();
    }
+   std::cout << "Cull mode: " << std::hex << viewer.getCamera()->getCullingMode() << std::endl;
+   viewer.getCamera()->setCullingMode(0);
 
    double frameTime = 0.0;
    double sumFrameTime = 0.0;
@@ -51,9 +114,70 @@ int main()
    while(!viewer.done()) {
       ::osg::Timer_t startFrameTick = ::osg::Timer::instance()->tick();
 
-      s1.assign(x1 + 0.5*(eval(x1&x1)-r*r*timer*timer)*einf + e0);
-      s1.update();
-      s1Drawable->dirtyDisplayList();
+      //s1.update(x1 + 0.5*(eval(x1&x1)-r*r*timer*timer)*einf + e0);
+      //s1Drawable->dirtyDisplayList();
+
+      double stroke = -0.24 + 0.01*timer;
+      double steer = 0.0;
+      auto x_wbf = 1.638*e1 + 0.333*e2 + (-0.488)*e3;
+      auto p_wbf = x_wbf + 0.5*(x_wbf&x_wbf)*einf + e0;
+      auto x_wbr = 1.4*e1 + 0.333*e2 + (-0.488)*e3;
+      auto p_wbr = x_wbr + 0.5*(x_wbr&x_wbr)*einf + e0;
+      auto x_mps = (1.335)*e1 + 0.473*e2 + (-0.043)*e3;
+      auto p_mps = x_mps + 0.5*(x_mps&x_mps)*einf + e0;
+      auto x_steer0 = 1.3*e1 + 0.141*e2 + (-0.388)*e3;
+      auto p_steer0 = x_steer0 + 0.5*(x_steer0&x_steer0)*einf + e0;
+      double side = 1.0;
+
+      stroke = std::max(-0.24, std::min(0.16, stroke));
+      steer = std::max(-0.05, std::min(0.05, steer));
+      //rod lengthes:
+      //mcpherson strut: spring and wheel carrier
+      double r_mps = 0.486 + stroke;
+      //wishbone
+      double r_wbf = 0.365;
+      double r_wbr = 0.23;
+
+      //wishbone circle
+      s_wbf.update(p_wbf - 0.5*r_wbf*r_wbf*einf);
+      s_wbf_Drawable->dirtyDisplayList();
+      s_wbr.update(p_wbr - 0.5*r_wbr*r_wbr*einf);
+      s_wbr_Drawable->dirtyDisplayList();
+      auto c_wb = s_wbf^s_wbr;
+
+      //sphere of mcpherson strut: spring and wheel carrier
+      s_mps.update(p_mps - 0.5*r_mps*r_mps*einf);
+      s_mps_Drawable->dirtyDisplayList();
+
+      //wheel carrier lower joint
+      auto Pp_wc = Ic * (c_wb^s_mps);
+      p_wc.update((Pp_wc + one*side*sqrt(eval(Pp_wc&Pp_wc))) * !(Pp_wc&einf)); //Point
+      p_wc_Drawable->dirtyDisplayList();
+
+      //steering link
+      double r_sl = 0.4;
+      //steering arm:
+      //from mcpherson struct dome
+      double r_samps = sqrt(pow(r_mps-0.15,2)+pow(0.12,2));
+      //from wheel carrier lower joint
+      double r_sawc = sqrt(pow(0.15,2)+pow(0.12,2));
+
+      //Translation induced to steering link inner joint by steering wheel (e.g. via a cograil)
+      auto T_steer = one - einf*(steer*e2)*0.5;
+      auto p_steer = grade<1>(T_steer*p_steer0*(~T_steer));
+
+      auto s_samps = p_mps - 0.5*r_samps*r_samps*einf;
+
+      auto s_sawc = p_wc - 0.5*r_sawc*r_sawc*einf;
+
+      auto s_steer = p_steer - 0.5*r_sl*r_sl*einf;
+
+      //steering arm
+      auto Pp_sa = (s_sawc^s_samps^s_steer) * Ic;
+      p_sa.update((Pp_sa - one*side*sqrt(eval(Pp_sa&Pp_sa))) * !(Pp_sa&einf)); //Point
+      p_sa_Drawable->dirtyDisplayList();
+
+
 
       viewer.frame();
 
--- cpp0x/examples/ClosedLoopKinematics/cga_osg.h
+++ cpp0x/examples/ClosedLoopKinematics/cga_osg.h
@@ -22,8 +22,8 @@ cga::mv<0x08, 0x10>::type einf = em+ep;
 
 cga::mv<0x18>::type E = ep*em;
 
-cga::mv<0x1f>::type I = e1*e2*e3*ep*em;
-cga::mv<0x07>::type i = e1*e2*e3;
+cga::mv<0x1f>::type Ic = e1*e2*e3*ep*em;
+cga::mv<0x07>::type Ie = e1*e2*e3;
 
 typedef cga::mv<0x03, 0x05, 0x06, 0x09, 0x0a, 0x0c, 0x11, 0x12, 0x14>::type S_type;
 typedef cga::mv<0x00, 0x03, 0x05, 0x06, 0x09, 0x0a, 0x0c, 0x0f, 0x11, 0x12, 0x14, 0x17>::type D_type;
@@ -61,25 +61,59 @@ namespace osg {
          setRadius(r);
       }
 
-      void update()
+      template<class E>
+      void update(const expression<E>& e_)
       {
+         this->multivector::operator=(multivector(e_));
+
          auto x = part<1,2,4>(*this)*(!((-1.0) * (*this & einf)));
          setCenter(::osg::Vec3(x.element<1>(), x.element<2>(), x.element<4>()));
          auto r = sqrt(eval((x&x) - 2.0*((-1.0) * (*this & e0))));
          setRadius(r);
-      }
-
-      void accept(::osg::ShapeVisitor& shapeVisitor)
-      {
-         auto x = part<1,2,4>(*this)*(!((-1.0) * (*this & einf)));
-         setCenter(::osg::Vec3(x.element<1>(), x.element<2>(), x.element<4>()));
-         auto r = sqrt(eval((x&x) - 2.0*((-1.0) * (*this & e0))));
-         setRadius(r);
-
-         ::osg::Sphere::accept(shapeVisitor);
       }
    };
 
+
+   class Point : public cga::mv<1,2,4,8,16>::type, public ::osg::Sphere
+   {
+   public:
+      //initialization
+      Point()
+         :  multivector({0.0,0.0,0.0,0.0,1.0}),
+            ::osg::Sphere()
+      {
+         setRadius(0.05);
+      }
+
+      Point(std::initializer_list<element_t> s)
+         : multivector(s),
+           ::osg::Sphere()
+      {
+         auto x = part<1,2,4>(*this)*(!((-1.0) * (*this & einf)));
+         setCenter(::osg::Vec3(x.element<1>(), x.element<2>(), x.element<4>()));
+         setRadius(0.05);
+      }
+
+      template<class E>
+      Point(const gaalet::expression<E>& e_) 
+         : multivector(e_),
+           ::osg::Sphere()
+      {
+         auto x = part<1,2,4>(*this)*(!((-1.0) * (*this & einf)));
+         setCenter(::osg::Vec3(x.element<1>(), x.element<2>(), x.element<4>()));
+         setRadius(0.05);
+      }
+
+      template<class E>
+      void update(const expression<E>& e_)
+      {
+         this->multivector::operator=(multivector(e_));
+
+         auto x = part<1,2,4>(*this)*(!((-1.0) * (*this & einf)));
+         setCenter(::osg::Vec3(x.element<1>(), x.element<2>(), x.element<4>()));
+      }
+
+   };
 } //end namspace osg
 
 
