c2f78e6d06df3b6d65f9d6fe5c868f90b38e45eb
groebiator <groebiator>
2011-02-22 16:44:47 +0000

    added more versions
    git-svn-id: https://svn.code.sf.net/p/gaalet/code/trunk@99 d266722a-31f7-4b50-8c79-7894b272e5be

---
+++ cpp0x/examples/InverseHandKinematics/InverseHandKinematicsLagrange.cpp
@@ -0,0 +1,167 @@
+#include "gaalet.h"
+
+#include <osgViewer/Viewer>
+#include <osgGA/StateSetManipulator>
+#include <osgGA/TrackballManipulator>
+#include <osg/PositionAttitudeTransform>
+#include <osg/ShapeDrawable>
+
+typedef gaalet::algebra<gaalet::signature<3,0,1>> ma;
+typedef ma::mv<0, 3, 5, 6, 9, 0xa, 0xc>::type Motor;
+
+int main()
+{
+   ma::mv<0>::type one={1.0};
+   ma::mv<1>::type e1={1.0};
+   ma::mv<2>::type e2={1.0};
+   ma::mv<4>::type e3={1.0};
+   ma::mv<8>::type e0={1.0};
+   ma::mv<0xf>::type I= (e1^e2^e3^e0);
+
+   double K_d_r = 10.0;
+   double K_d_t = 10.0;
+   double K_1 = 1.0;
+   double K_2 = 1.0;
+
+   auto T_t = one + 0.5*I*(10.0*e2*e3 + 0.0*e3*e1 + -10.0*e1*e2);
+   double phi_t = 0.25*M_PI;
+   auto R_t = one*cos(-phi_t*0.5) + sin(-phi_t*0.5)*(0.0*e2*e3 + 1.0*e3*e1 + 0.0*e1*e2);
+   auto M_t = eval(T_t*R_t);
+
+   //Ball joint R_1:
+   auto T_1 = eval(one + 0.5*I*(7.0*e2*e3 + 0.0*e3*e1 + 0.0*e1*e2));
+   double phi_1 = -0.2*M_PI;
+   auto R_1_0 = eval(one*cos(-phi_1*0.5) + sin(-phi_1*0.5)*(0.0*e2*e3 + 1.0*e3*e1 + 0.0*e1*e2));
+
+   ma::mv<3,5,6>::type S_1;
+
+   //Ball joint R_2:
+   auto T_2 = eval(one + 0.5*I*(7.0*e2*e3 + 0.0*e3*e1 + 0.0*e1*e2));
+   double phi_2 = 0.4*M_PI;
+   auto R_2_0 = eval(one*cos(-phi_2*0.5) + sin(-phi_2*0.5)*(0.0*e2*e3 + 1.0*e3*e1 + 0.0*e1*e2));
+
+   ma::mv<3,5,6>::type S_2;
+
+   double lambda = 1.0;
+
+   //Visualisation with OpenSceneGraph
+   osg::Group* sceneRoot = new osg::Group;
+
+   auto revconjM_t = M_t;
+   revconjM_t[1] = -revconjM_t[1];
+   revconjM_t[2] = -revconjM_t[2];
+   revconjM_t[3] = -revconjM_t[3];
+   auto X_s = eval(M_t*revconjM_t);
+
+   osg::Sphere* sphere = new osg::Sphere(osg::Vec3(-X_s[4], -X_s[5], -X_s[6]), 0.3);
+   osg::ShapeDrawable* sphereDrawable = new osg::ShapeDrawable(sphere);
+   osg::Geode* sphereGeode = new osg::Geode();
+   sphereGeode->addDrawable(sphereDrawable);
+   sceneRoot->addChild(sphereGeode);
+
+   osg::Box* rod1 = new osg::Box(osg::Vec3(3.5, 0.0, 0.0), 7.0, 0.2, 0.2);
+   osg::ShapeDrawable* rod1Drawable = new osg::ShapeDrawable(rod1);
+   osg::Geode* rod1Geode = new osg::Geode();
+   rod1Geode->addDrawable(rod1Drawable);
+   osg::PositionAttitudeTransform* rod1Transform = new osg::PositionAttitudeTransform();
+   rod1Transform->addChild(rod1Geode);
+   sceneRoot->addChild(rod1Transform);
+
+   osg::Box* rod2 = new osg::Box(osg::Vec3(3.5, 0.0, 0.0), 7.0, 0.2, 0.2);
+   osg::ShapeDrawable* rod2Drawable = new osg::ShapeDrawable(rod2);
+   osg::Geode* rod2Geode = new osg::Geode();
+   rod2Geode->addDrawable(rod2Drawable);
+   osg::PositionAttitudeTransform* rod2Transform = new osg::PositionAttitudeTransform();
+   rod2Transform->addChild(rod2Geode);
+   sceneRoot->addChild(rod2Transform);
+
+   osgViewer::Viewer viewer;
+   viewer.setSceneData( sceneRoot );
+   viewer.addEventHandler( new osgGA::StateSetManipulator(viewer.getCamera()->getOrCreateStateSet()) );
+   if (!viewer.getCameraManipulator() && viewer.getCamera()->getAllowEventFocus())
+   {
+      viewer.setCameraManipulator(new osgGA::TrackballManipulator());
+   }
+   viewer.setReleaseContextAtEndOfFrameHint(false);
+
+   if (!viewer.isRealized())
+   {
+      viewer.realize();
+   }
+
+
+   //double alpha_0 = 0.001;
+   //for(int i=0; i<100; ++i) {
+   double frameTime = 0.0;
+   double sumFrameTime = 0.0;
+   double minFrameTime = 0.1;
+   double timer = 0.0;
+   unsigned int counter = 0;
+   while(!viewer.done()) {
+      osg::Timer_t startFrameTick = osg::Timer::instance()->tick();
+
+
+      auto R_1_d = exp(0.5*S_1);
+      auto R_2_d = exp(0.5*S_2);
+      double alpha = 0.01;
+
+      auto G = eval(R_1_d*R_1_0*T_1*R_2_d*R_2_0*T_2-M_t);
+
+      //Ball joint R_1:
+      auto grad_S1_U = K_1*S_1 + lambda*G*M_t;
+
+      S_1 = S_1 - alpha*grad_S1_U;
+
+      //Ball joint R_2:
+      auto grad_S2_U = K_2*S_2 + lambda*G*M_t;
+
+      S_2 = S_2 - alpha*grad_S2_U;
+
+      //Lambda
+      lambda = lambda - eval(alpha*grade<0>(G*G));
+
+      auto M_1 = eval(R_1_d*R_1_0*T_1);
+      auto M_2 = eval(R_2_d*R_2_0*T_2);
+      auto M_12 = eval(M_1*M_2);
+      auto revconjM_1 = M_1;
+      revconjM_1[1] = -revconjM_1[1];
+      revconjM_1[2] = -revconjM_1[2];
+      revconjM_1[3] = -revconjM_1[3];
+      auto X_1 = eval(M_1*revconjM_1);
+      auto revconjM_12 = M_12;
+      revconjM_12[1] = -revconjM_12[1];
+      revconjM_12[2] = -revconjM_12[2];
+      revconjM_12[3] = -revconjM_12[3];
+      auto X_2 = eval(M_12*revconjM_12);
+      //std::cout << "X_1: " << X_1 << ", X_2: " << X_2 << std::endl;
+      rod1Transform->setAttitude(osg::Quat(-M_1[3], M_1[2], -M_1[1], M_1[0]));
+      rod2Transform->setPosition(osg::Vec3d(-X_1[4], -X_1[5], -X_1[6]));
+      rod2Transform->setAttitude(osg::Quat(-M_12[3], M_12[2], -M_12[1], M_12[0]));
+
+      std::cout << "i=" << counter << ": S_1: " << S_1 << ", S_2: " << S_2 << std::endl;
+      std::cout << "\tgrad_S1_U: " << grad_S1_U << std::endl;
+      std::cout << "\tgrad_S2_U: " << grad_S2_U << std::endl;
+
+      viewer.frame();
+
+      //work out if we need to force a sleep to hold back the frame rate
+      osg::Timer_t endFrameTick = osg::Timer::instance()->tick();
+      frameTime = osg::Timer::instance()->delta_s(startFrameTick, endFrameTick);
+
+      sumFrameTime += frameTime;
+      if(counter==1000) {
+         std::cout << "Average frame time: " << sumFrameTime/1000.0 << std::endl;
+         sumFrameTime = 0.0;
+         counter = 0;
+      }
+      else {
+         counter++;
+      }
+
+      timer += frameTime;
+
+      if (frameTime < minFrameTime) OpenThreads::Thread::microSleep(static_cast<unsigned int>(1000000.0*(minFrameTime-frameTime)));
+
+   }
+
+}
