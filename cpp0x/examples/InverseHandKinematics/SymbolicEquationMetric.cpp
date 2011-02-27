cd047717f0dd77bc299f5de030ac57da44a0190a
groebiator <groebiator>
2011-02-27 23:19:30 +0000

    some efforts
    git-svn-id: https://svn.code.sf.net/p/gaalet/code/trunk@107 d266722a-31f7-4b50-8c79-7894b272e5be

--- cpp0x/examples/InverseHandKinematics/SymbolicEquationMetric.cpp
+++ cpp0x/examples/InverseHandKinematics/SymbolicEquationMetric.cpp
@@ -10,7 +10,9 @@ int main() {
    sem::mv<3,5,6>::type m = {"m12", "m13", "m23"};
    auto mag_m = magnitude(m);
    auto R = eval(cos(eval((-0.5)*mag_m))*one + m*!mag_m*sin(eval((-0.5)*mag_m)));
+   //std::cout << "R*~R: " << grade<0>(R*~R) << std::endl;
+   std::cout << "t&(R*x*~R): " << grade<0>(t&(R*x*~R)) << std::endl;
 
    auto U = eval(0.5*(t - grade<1>(R*x*~R))&(t - grade<1>(R*x*~R)));
-   std::cout << "U: " << U << std::endl;
+   //std::cout << "U: " << U << std::endl;
 }
