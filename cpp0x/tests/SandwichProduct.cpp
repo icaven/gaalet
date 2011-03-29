d068eb1022ecdd21aae71c38f31301e63ad73857
groebiator <groebiator>
2011-03-29 11:12:13 +0000

    git-svn-id: https://svn.code.sf.net/p/gaalet/code/trunk@111 d266722a-31f7-4b50-8c79-7894b272e5be

--- cpp0x/tests/SandwichProduct.cpp
+++ cpp0x/tests/SandwichProduct.cpp
@@ -15,6 +15,31 @@ auto sandwich(const gaalet::expression<L>& l_, const gaalet::expression<R>& r_) 
    return (r*l*(~r));
 }*/
 
+namespace gaalet {
+
+template<class L, class R>
+struct geometric_product<L, geometric_product<R, reverse<L>>> : public expression<geometric_product<L, geometric_product<R, reverse<L>>>>
+{
+   typedef typename element_type_combination_traits<typename L::element_t, typename R::element_t>::element_t element_t;
+   typedef typename metric_combination_traits<typename L::metric, typename R::metric>::metric metric;
+
+   typedef typename R::clist clist;
+
+   geometric_product(const L& l_ , const R& r_)
+      :  l(l_), r(r_)
+   { }
+
+   template<conf_t conf>
+   element_t element() const {
+      return -1;
+   }
+
+protected:
+   const L& l;
+   const R& r;
+};
+
+}
 
 typedef gaalet::algebra< gaalet::signature<3,0> > em;
 
@@ -29,4 +54,8 @@ int main()
 
    const auto& s = sandwich(v,R);
    std::cout << "sandwich(v,R): " << s << std::endl;
+
+   auto a = R*(v*~R);
+   
+   std::cout << "a: " << a << std::endl;
 }
