7f389d55e18b6860205200a2e02a862767b96b7e
groebiator <groebiator>
2011-04-29 12:01:28 +0000

    generalized/templated streaming operator for gaalet expressions
    git-svn-id: https://svn.code.sf.net/p/gaalet/code/trunk@115 d266722a-31f7-4b50-8c79-7894b272e5be

--- include/cpp0x/streaming.h
+++ include/cpp0x/streaming.h
@@ -5,25 +5,28 @@
 #include "multivector.h"
 
 //expression streaming
-template<typename E, typename clist>
+template<typename G, typename clist>
 struct UnpackElementsToStream
 {
-   static void unpack(std::ostream& os, const E& e) {
+   template<class E, class T>
+   static void unpack(std::basic_ostream<E, T>& os, const G& e) {
       os << e.template element<clist::head>() << " ";
-      UnpackElementsToStream<E, typename clist::tail>::unpack(os, e);
+      UnpackElementsToStream<G, typename clist::tail>::unpack(os, e);
    }
 };
 
-template<typename E>
-struct UnpackElementsToStream<E, gaalet::cl_null>
+template<typename G>
+struct UnpackElementsToStream<G, gaalet::cl_null>
 {
-   static void unpack(std::ostream&, const E&) { }
+   template<class E, class T>
+   static void unpack(std::basic_ostream<E, T>&, const G&) { }
 };
 
 template<typename clist>
 struct UnpackConfigurationListToStream
 {
-   static void unpack(std::ostream& os) {
+   template<class E, class T>
+   static void unpack(std::basic_ostream<E, T>& os) {
       os << clist::head << " ";
       UnpackConfigurationListToStream<typename clist::tail>::unpack(os);
    }
@@ -32,19 +35,20 @@ struct UnpackConfigurationListToStream
 template<>
 struct UnpackConfigurationListToStream<gaalet::cl_null>
 {
-   static void unpack(std::ostream&) { }
+   template<class E, class T>
+   static void unpack(std::basic_ostream<E, T>&) { }
 };
 
-template<class E>
-std::ostream& operator<<(std::ostream& os, const gaalet::expression<E>& e_)
+template<class E, class T, class G>
+std::basic_ostream<E, T>& operator<<(std::basic_ostream<E, T>& os, const gaalet::expression<G>& e_)
 {
-   const E& e(e_);
+   const G& e(e_);
    //auto mv = eval(e_);
 
    os << "[ " << std::dec;
-      UnpackElementsToStream<E, typename E::clist>::unpack(os, e);
+      UnpackElementsToStream<G, typename G::clist>::unpack(os, e);
    os << "] { " << std::hex;
-      UnpackConfigurationListToStream<typename E::clist>::unpack(os);
+      UnpackConfigurationListToStream<typename G::clist>::unpack(os);
    os << '}';
 
    return os;
