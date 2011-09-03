b4bf56166ccbcc2a8a0acaad840f2ab77f32ca16
groebiator <groebiator>
2011-09-03 13:13:33 +0000

    some small cosmetics
    git-svn-id: https://svn.code.sf.net/p/gaalet/code/trunk@131 d266722a-31f7-4b50-8c79-7894b272e5be

--- cpp0x/tests/Rotation.cpp
+++ cpp0x/tests/Rotation.cpp
@@ -1,18 +1,22 @@
 #include "gaalet.h"
 
-typedef gaalet::algebra<gaalet::signature<3,0>> em;
-static em::mv<0x01>::type e1 = {1.0};
-static em::mv<0x02>::type e2 = {1.0};
-static em::mv<0x04>::type e3 = {1.0};
+typedef gaalet::algebra<gaalet::signature<3,0,0>> em;
+
+typedef em::mv<0x1,0x2,0x4>::type Vector;
+
+static em::mv<0x1>::type e1 = {1.0};
+static em::mv<0x2>::type e2 = {1.0};
+static em::mv<0x4>::type e3 = {1.0};
 
 int main()
 {
-   auto S = -0.5*M_PI*(e1^e2);
-   auto R = exp(0.5*S);
+   auto m = 0.5*M_PI*(e1^e2);
+   auto R = eval(exp(-0.5*m));
 
-   em::mv<1,2,4>::type a = {1.6, 3.2, 5.5}; 
+   Vector a = {1.6, 3.2, 5.5}; 
    auto b = eval(grade<1>(R*a*(~R)));
 
-   std::cout << "S: " << S << ", R: " << R << std::endl;
+   std::cout << "m: " << m << ", R: " << R << std::endl;
    std::cout << "a: " << a << ", b: " << b << std::endl;
 }
+
